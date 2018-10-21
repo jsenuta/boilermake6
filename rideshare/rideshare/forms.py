6efccc789e21823920eb8ac803d235770c6d388a
Christine Wang <wangchrstne@gmail.com>
2018-10-20 20:11:17 -0400

    added post list

--- rideshare/rideshare/forms.py
+++ rideshare/rideshare/forms.py
@@ -6,4 +6,4 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('author', 'startLoc','endLoc','seats','date_ride','published_date')
+        fields = ('author', 'startLoc','endLoc','seats')
--- rideshare/rideshare/views.py
+++ rideshare/rideshare/views.py
@@ -1,6 +1,9 @@
 #import pyrebase
 from django.shortcuts import render
 from .forms import PostForm
+from django.shortcuts import redirect
+from django.utils import timezone
+from .models import Post
 
 config = {
     'apiKey': "AIzaSyDI2z3pBo_2Fk4itX3CIg72XX8sMqsqalc",
@@ -32,9 +35,18 @@ def ridepost(request):
 def viewRides(request):
     return render(request, "viewRides.html", {"request":request})
 def post_list(request):
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'post_list.html')
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return render(request, 'post_list.html')
+    else:
+        form = PostForm()
     return render(request, 'post_edit.html', {'form': form})
-
 
